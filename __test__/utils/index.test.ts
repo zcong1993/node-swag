c31bf6557c0c9c36dd6d537b5b0449f269777272
zcong1993 <1005997919@qq.com>
2019-01-24 03:12:17 +0800

    update test

--- __test__/utils/index.test.ts
+++ __test__/utils/index.test.ts
@@ -2,24 +2,61 @@ import { normalizeConfig } from '../../src/utils'
 import { RootConfig } from '../../src/types'
 
 it('should works well', () => {
-  const sample: RootConfig = {
-    host: 'localhost:8080',
-    basePath: '/v1',
-    files: ['./src/*.js'],
-    model: 'test.ts',
-    outType: 'json'
-  }
+  const samples: RootConfig[] = [
+    {
+      host: 'localhost:8080',
+      basePath: '/v1',
+      files: ['./src/*.js'],
+      model: 'test.ts',
+      outType: 'json'
+    },
+    {
+      host: 'localhost:8080',
+      files: ['./src/*.js'],
+      model: 'test.ts',
+      outType: 'json'
+    },
+    {
+      host: 'localhost:8080',
+      basePath: '/v1',
+      files: ['./src/*.js'],
+      model: 'test.ts',
+      outType: 'json',
+      info: {}
+    },
+    {
+      host: 'localhost:8080',
+      files: ['./src/*.js'],
+      model: 'test.ts',
+      outType: 'yaml'
+    },
+    {
+      host: 'localhost:8080',
+      files: ['./src/*.js'],
+      model: 'test.ts',
+      outType: 'json',
+      outDir: './out'
+    }
+  ]
 
-  expect(normalizeConfig(sample)).toMatchSnapshot()
+  samples.forEach(sample => expect(normalizeConfig(sample)).toMatchSnapshot())
 })
 
 it('should throw error', () => {
-  const sample = {
-    basePath: '/v1',
-    files: ['./src/*.js'],
-    model: 'test.ts',
-    outType: 'json'
-  }
+  const samples = [
+    {
+      basePath: '/v1',
+      files: ['./src/*.js'],
+      model: 'test.ts',
+      outType: 'json'
+    },
+    {
+      host: 'localhost:8080',
+      basePath: '/v1',
+      model: 'test.ts',
+      outType: 'json'
+    }
+  ]
 
-  expect(() => normalizeConfig(sample as RootConfig)).toThrow()
+  samples.forEach(sample => expect(() => normalizeConfig(sample as RootConfig)).toThrow())
 })
